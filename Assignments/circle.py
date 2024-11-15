8a8c6404099ad817cf9056a51a0447e8ba08184e
Stavoh <stavoedu@gmail.com>
2024-11-15 10:06:43 +0200

    Add files via upload

---
+++ Assignments/circle.py
@@ -0,0 +1,11 @@
+## Assignment 2b
+
+# Write a python program that has one variable radius of a circle. Print out the area and the circumference of the circle. (cricle.py)
+
+radius= 10
+π=3.141592653589793
+
+circumference=2*π*radius
+area= π*(radius^2)
+
+print("Area: ", area, ". Circumference: ", circumference, ".")
--- Assignments/rectangle.py
+++ Assignments/rectangle.py
@@ -1,1 +1,12 @@
+## Assignment 2a
 
+# Write a python program that has two variables width and length of a rectangle. Print out the area and the circumference of the rectangle. (rectangle.py)
+
+width=11
+length=22
+
+area=width*length
+
+circumference=2*(width+length)
+
+print("Area: ", area, ". Circumference: ", circumference, ".")
