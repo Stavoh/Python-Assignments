96a61ccb0990e32caf849bda59602a0a19a4da73
Stavoh <stavoedu@gmail.com>
2024-11-24 12:17:29 +0200

    add day3

---
+++ day03/circle.py
@@ -0,0 +1,13 @@
+## Assignment 3
+
+# Get the user to input the value.
+
+print("Choose radius:")
+radius=int(input())
+
+π=3.141592653589793
+
+circumference=2*π*radius
+area= π*(radius^2)
+
+print("Area: ", area, ". Circumference: ", circumference, ".")
---
+++ day03/rectangle.py
@@ -0,0 +1,16 @@
+## Assignment 3
+
+# Get the user to input the values.
+
+print("Choose width:")
+width=int(input())
+
+print("Choose length:")
+length=int(input())
+
+
+area=width*length
+
+circumference=2*(width+length)
+
+print("Area: ", area, ". Circumference: ", circumference, ".")
